1a253e9871f9c1fb7e4f344c492e8c25e92a014a
1kamson2 <qwerty21028@gmail.com>
2025-06-28 22:15:23 +0200

    xd420

--- Models/Database.cs
+++ Models/Database.cs
@@ -21,14 +21,29 @@ namespace Database
             connInfo = $"Server={DbAddress};Port={Port};Database={DbName};Uid={UserName};Pwd={Password};";
         }
 
-
+        public void LoginUser(string[] userData)
+        {
+            string userLoginQuery = String.Format(
+                    "SELECT user_in_db({0}, {1})", userData[0], userData[1]
+                    );
+            MakeQuery(userLoginQuery);
+        }
 
         public void RegisterUser(string[] userData)
         {
-
+            string userAddQuery = String.Format(
+                    "CALL add_user_full_info({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8})",
+                    userData[0],
+                    userData[1],
+                    userData[2],
+                    userData[3],
+                    userData[4],
+      userData[5],
+                    userData[6],
+                    userData[7],
+                    userData[8]);
+            MakeQuery(userAddQuery);
         }
-
-
 
         private void MakeQuery(string query)
         {
